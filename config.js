a0628e24833f515df9e52c60e1d7e6a7324ca858
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-11-04 17:39:14 -0700

    comment out audience

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const config = {
 		...defaultAuthConfig,
 		domain: 'atko-rocks-gentle-animal.demo-platform-staging.auth0app.com',
 		client_id: 'RBz9va21UvCeuSTYT9nMoRTZah1iTnoH',
-		// UNCOMMENT LINE 12 for STEP X
-		audience: ['api://authrocks'],
+		// UNCOMMENT LINE 12 to test the private API
+		// audience: ['api://authrocks'],
 	},
 	app: {
 		port: 3000,
