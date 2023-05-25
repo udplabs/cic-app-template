417d661aa72347bf7759f23f14b09d3f7d61d8e1
eatplaysleep <eatplaysleep@gmail.com>
2023-05-24 18:55:58 -0600

    refactoring

--- app/utils.js
+++ app/utils.js
@@ -1,114 +1,67 @@
-import { router } from './index.js';
-import config from '../config';
+import { eachElement, elementMapper, parseJwt } from '../utils';
 
-export const assert = (condition, message = 'Assertion failed!') => {
-	if (!condition) {
-		throw new Error(message);
-	}
+export var authState = {
+    accessToken: undefined,
+    isAuthenticated: false,
+    user: undefined,
 };
 
-/**
- * Iterates over the elements matching 'selector' and passes them
- * to 'fn'
- * @param {*} selector The CSS selector to find
- * @param {*} fn The function to execute for every element
- */
-export const eachElement = (selector, fn) => {
-	for (let e of document.querySelectorAll(selector)) {
-		fn(e);
-	}
-};
+export const authStateProvider = new Proxy(authState, {
+    set: (target, key, value) => {
+        target[key] = value;
 
-export const elementMapper = {
-	user: 'ipt-user-profile',
-	accessToken: 'ipt-access-token',
-	loadingMsg: 'loading-msg',
-	loadingTitle: 'loading-title',
-};
+        switch (key) {
+            case 'accessToken':
+                value = value ? parseJwt(value) : '';
 
-/**
- * Returns true if `element` is a hyperlink that can be considered a link to another SPA route
- * @param {*} element The element to check
- */
-export const isRouteLink = (element) => element.tagName === 'A' && element.classList.contains('route-link');
+                document.getElementById(elementMapper[key]).innerHTML = JSON.stringify(
+                    value,
+                    null,
+                    4
+                );
+                break;
+            case 'isAuthenticated':
+                eachElement('.auth-invisible', (e) =>
+                    e.classList[value ? 'add' : 'remove']('auth-hidden')
+                );
 
-export const getConfig = () => {
-	const _currentConfigBase64 = localStorage.getItem('config');
-	let _currentConfig;
-	let configBase64;
+                eachElement('.auth-visible', (e) =>
+                    e.classList[value ? 'remove' : 'add']('auth-hidden')
+                );
 
-	if (_currentConfigBase64 && !['undefined', 'null'].includes(_currentConfigBase64)) {
-		_currentConfig = window.atob(_currentConfigBase64);
+                if (!value) {
+                    target.accessToken = undefined;
+                    target.user = undefined;
+                }
+                break;
+            case 'user':
+                document
+                    .querySelectorAll(`[id^=${elementMapper[key]}]`)
+                    .forEach(
+                        (element) =>
+                            (element.innerHTML = !value ? '' : JSON.stringify(value, null, 4))
+                    );
 
-		if (_currentConfig) {
-			_currentConfig = JSON.parse(_currentConfig);
-		}
-	}
+                eachElement(
+                    '.profile-image',
+                    (element) => (element.src = value?.picture || '')
+                );
+                eachElement(
+                    '.user-name',
+                    (element) => (element.innerText = value?.name || '')
+                );
+                eachElement(
+                    '.user-email',
+                    (element) => (element.innerText = value?.email || '')
+                );
 
-	if (config && config?.auth?.clientId !== '_CLIENTID_' && config?.auth?.domain !== '_DOMAIN_') {
-		if (config?.auth?.audience?.includes('_AUDIENCE_')) {
-			delete config.auth.audience;
-		}
+                break;
+            default:
+                break;
+        }
 
-		configBase64 = window.btoa(JSON.stringify(config));
-	}
+        // document.querySelectorAll('pre code').forEach(hljs.highlightBlock);
 
-	let result = { hasChanged: false, config };
-
-	result.hasChanged = _currentConfigBase64 !== configBase64;
-
-	if (!_currentConfigBase64 || result.hasChanged) {
-		localStorage.setItem('config', configBase64);
-	}
-
-	console.group('=== config ===');
-	console.log(JSON.stringify(result, null, 2));
-	console.groupEnd();
-
-	return result;
-};
-
-export const parseJwt = (token) => {
-	var base64Url = token.split('.')[1];
-	var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-	if (base64 == '') {
-		return false;
-	}
-	var jsonPayload = decodeURIComponent(
-		window
-			.atob(base64)
-			.split('')
-			.map(function (c) {
-				return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-			})
-			.join('')
-	);
-
-	return JSON.parse(jsonPayload);
-};
-
-/**
- * Displays a content panel specified by the given element id.
- * All the panels that participate in this flow should have the 'page' class applied,
- * so that it can be correctly hidden before the requested content is shown.
- * @param {*} id The id of the content to show
- */
-export const showContent = (id) => {
-	eachElement('.page', (p) => p.classList.add('hidden'));
-	document.getElementById(id).classList.remove('hidden');
-};
-
-/**
- * Tries to display a content panel that is referenced
- * by the specified route URL. These are matched using the
- * router, defined above.
- * @param {*} url The route URL
- */
-export const showContentFromUrl = (url) => {
-	if (router[url]) {
-		router[url]();
-		return true;
-	}
-
-	return false;
-};
+        return true;
+    },
+});
--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,29 @@ import mixPlugin from 'vite-plugin-mix';
 const mix = mixPlugin.default;
 
 export default defineConfig(({ command, mode }) => {
-	Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
-	const { VITE_APP_PORT: port, APP_ENV } = process?.env;
+    Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+    const { VITE_APP_PORT: port, APP_ENV } = process?.env;
 
-	return {
-		plugins: [
-			mix({ handler: './server.js' })
-		],
-		define: {
-			__APP_ENV__: APP_ENV,
-		},
-		server: {
-			port,
-		},
-		build: {
-			// generate manifest.json in outDir
-			manifest: true,
-			modulePreload: {
-				polyfill: false,
-			},
-			rollupOptions: {
-				// overwrite default .html entry
-				input: './main.js',
-			},
-		},
-	};
+    return {
+        plugins: [
+            mix({ handler: './server.js' })
+        ],
+        define: {
+            __APP_ENV__: APP_ENV,
+        },
+        server: {
+            port,
+        },
+        build: {
+            // generate manifest.json in outDir
+            manifest: true,
+            modulePreload: {
+                polyfill: false,
+            },
+            rollupOptions: {
+                // overwrite default .html entry
+                input: './main.js',
+            },
+        },
+    };
 });
