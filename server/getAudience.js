ace813b046a8a841ee8b643af3f239aadb5ac38c
Danny Fuhriman <danny.fuhriman@Okta.com>
2023-06-20 13:21:49 -0700

    fix getAudience

--- server/getAudience.js
+++ server/getAudience.js
@@ -2,15 +2,8 @@ const { SERVER_AUDIENCE: AUDIENCE = [] } = process.env;
 
 /**
  * Fetches the `audience` value from configuration (if not provided) and parses it appropriately to avoid user input errors.
- * @param {string | string[]} audience
- * @returns {string[]}
+ * @param {string | string[] | []} audience
+ * @returns {string[] | []}
  */
-export const getAudience = (audience = AUDIENCE) => {
-	if (!Array.isArray(audience)) {
-		audience = [audience];
-	}
-
-	audience = audience?.includes(', ') ? audience?.split(', ') : audience?.split(',');
-
-	return audience;
-};
+export default (audience = AUDIENCE) =>
+	Array.isArray(audience) ? audience : audience?.includes(', ') ? audience?.split(', ') : audience?.split(',');
